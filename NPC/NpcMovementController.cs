087ea7a48e92c60ba61c48fc0547011b0ce32418
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-11-08 01:42:43 -0500

    womp womp

---
+++ NPC/NpcMovementController.cs
@@ -0,0 +1,47 @@
+﻿using Godot;
+using ShroomJamGame.CharacterBody;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShroomJamGame.NPC
+{
+
+    [GlobalClass]
+    public partial class NpcMovementController : Node
+    {
+        [Export]
+        private CharacterMovementController? _characterMovementController;
+        [Export]
+        public Node3D? targetNode;
+        [Export]
+        private CharacterBody3D? characterBody3D;
+        [Export]
+        private NavigationAgent3D? _navigationAgent;
+
+        public void _SetTargetPosition(Vector3 position)
+        {
+            _navigationAgent.TargetPosition = position;
+        }
+        private Vector3 oldPlayerPos = Vector3.Zero;
+        public override void _PhysicsProcess(double delta)
+        {
+            if (oldPlayerPos != targetNode.GlobalPosition)
+            {
+                oldPlayerPos = targetNode.GlobalPosition;
+                _SetTargetPosition(oldPlayerPos);
+            }
+            if (_navigationAgent.IsNavigationFinished())
+            {
+                return;
+            }
+            Vector3 nextPathPosition = _navigationAgent.GetNextPathPosition();
+            Vector3 movementDirection = characterBody3D.GlobalPosition.DirectionTo(nextPathPosition);
+            characterBody3D.LookAt(nextPathPosition);
+            characterBody3D.RotationDegrees = new Vector3(0, characterBody3D.RotationDegrees.Y + 180, 0);
+            _characterMovementController.InputMovement = new Vector2(0,1);
+        }
+    }
+}
