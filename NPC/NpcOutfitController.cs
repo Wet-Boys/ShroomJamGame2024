55404927529e85e13cf80bc8c530a330431bba83
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-11-08 17:46:42 -0500

    working on npc outfits

---
+++ NPC/NpcOutfitController.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShroomJamGame.NPC
+{
+    [GlobalClass]
+    public partial class NpcOutfitController : Node
+    {
+        [Export]
+        public NpcOutfits outfitList;
+        [Export]
+        public MeshInstance3D feet;
+        [Export]
+        public MeshInstance3D hands;
+        [Export]
+        public MeshInstance3D torso;
+        [Export]
+        public MeshInstance3D head;
+
+        public void RandomizeParts()
+        {
+            feet.Mesh = outfitList.feetMeshes.PickRandom();
+            hands.Mesh = outfitList.handMeshes.PickRandom();
+            torso.Mesh = outfitList.torsoMeshes.PickRandom();
+            head.Mesh = outfitList.headMeshes.PickRandom();
+        }
+    }
+}
---
+++ NPC/NpcVisualController.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShroomJamGame.NPC
+{
+    [GlobalClass]
+    public partial class NpcVisualController : Node3D
+    {
+        [Export]
+        NpcOutfitController outfitController;
+        [Export]
+        AnimationTree animationTree;
+        Tween tween;
+
+        public void SetAnimationTreeState(float walkState)
+        {
+            if (tween is not null)
+            {
+                tween.Kill();
+            }
+            tween = CreateTween();
+            tween.TweenProperty(animationTree, "parameters/movement_blend/blend_position", walkState, .25f);
+        }
+        public void RandomizeOutfit()
+        {
+            outfitController.RandomizeParts();
+        }
+    }
+}
