3b926c880beb25c9d7b4aea80be1fa566e82ebdd
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2024-11-13 18:29:58 -0500

    working on suffering

--- Tasks/FixManyComputers.cs
+++ Tasks/FixManyComputers.cs
@@ -1,6 +1,8 @@
 ﻿using Godot;
+using ShroomJamGame.Corruption;
 using ShroomJamGame.Events;
 using ShroomJamGame.Interaction;
+using ShroomJamGame.Rendering.Effects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +14,12 @@ namespace ShroomJamGame.Tasks
     public partial class FixManyComputers : BaseTask//many monsters here this door many monsters this door many monsters this door many monsters this door many monsters this door many monsters this door many monsters this door many monsters this door
     {
         Godot.Collections.Array<PhysicsInteractable> interactables = new Godot.Collections.Array<PhysicsInteractable>();
+        Godot.Collections.Array<Node3D> arrows = new Godot.Collections.Array<Node3D>();
         int tracker = 0;
         public override BaseTask Setup(Node worldRoot)
         {
             taskName = "Go fix it all";
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
                 PhysicsInteractable yeet;
                 do
@@ -30,8 +33,14 @@ namespace ShroomJamGame.Tasks
                 item.holdable = false;
                 BroadCastHandler.instance.HighlightObject(item);
                 item.nonHoldableText = "Repair it";
+                arrows.Add(((PackedScene)GD.Load("res://Assets/Prefabs/DestinationMarker.tscn")).Instantiate<Node3D>());
+                arrows.Last().Scale *= .3f;
+                arrows.Last().Position += new Vector3(0, .5f, 0);
+                item.AddChild(arrows.Last());
                 item.interactionFunction = (yote) =>
                 {
+                    arrows[interactables.IndexOf(item)].QueueFree();
+                    arrows.RemoveAt(interactables.IndexOf(item));
                     interactables.Remove(item);
                     BroadCastHandler.instance.UnHighlightObject(item);
                     tracker++;
@@ -46,7 +55,7 @@ namespace ShroomJamGame.Tasks
             }
             return base.Setup(worldRoot);
         }
-
+        bool ending = false;
         private void FixManyComputers_ProgressBarFinished(Node3D bar)
         {
             taskName = $"Go fix it all, items left: {interactables.Count}";
@@ -56,10 +65,28 @@ namespace ShroomJamGame.Tasks
                 default:
                     break;
             }
-            if (interactables.Count == 0)
+            if (interactables.Count == 0 && !ending)
             {
+                ending = true;
                 EmitSignal(SignalName.TaskFinished, this);
+                TaskTracker.instance.personalAudio.Stream = (AudioStreamOggVorbis)GD.Load("res://Assets/Sfx/Vinny/manualBlast.ogg");
+                TaskTracker.instance.personalAudio.Play();
+                CrashGame();
             }
+        }
+        private async void CrashGame()
+        {
+            await ToSignal(TaskTracker.instance.GetTree().CreateTimer(6.4f), SceneTreeTimer.SignalName.Timeout);
+            AudioCrash.Instance.StartAudioCrash();
+            FrameCaptureEffect.CaptureNextFrame = true;
+            DataMoshEffect.Instance.Enabled = true;
+            DataMoshEffect.Instance.Blend = 1;
+            await ToSignal(TaskTracker.instance.GetTree().CreateTimer(5f), SceneTreeTimer.SignalName.Timeout);
+            AudioCrash.Instance.StopAudioCrash();
+            DataMoshEffect.Instance.Enabled = false;
+            TaskTracker.instance.personalAudio.Stream = (AudioStreamOggVorbis)GD.Load("res://Assets/Sfx/glassShatter.ogg");
+            TaskTracker.instance.personalAudio.Play();
+            TaskTracker.instance.HUD.GetNode<Control>("CrashScreen").Visible = true;
         }
     }
 }
